a993e4064e22133102bc5364653363773c01a2e0
root <root@LAPTOP-MHOBOSL3.localdomain>
2020-03-31 18:39:17 +0700

    create and show comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "blog#index"
-  resources :blog
+  resources :blog do
+    resources :comments
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
