e3ffa3fef8a80696c852bae457ddaed5d6390be3
root <root@LAPTOP-MHOBOSL3.localdomain>
2020-04-03 16:10:20 +0700

    fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,7 +12,7 @@ require("@rails/activestorage").start()
 require("channels")
 
 import "bootstrap"
-import "../stylesheets/application"
+import "../../stylesheets/application"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
